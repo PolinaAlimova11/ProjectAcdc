a46b811da29a6a7136c399bd89b87e838c23f5be
PolinaAlimova11 <catcheshire07@gmail.com>
2024-06-20 21:16:02 +0300

    add test container

--- src/test/java/com/javarush/alimova/ConfigurationIT.java
+++ src/test/java/com/javarush/alimova/ConfigurationIT.java
@@ -5,7 +5,10 @@ import com.javarush.alimova.configure.ConfigurationDB;
 import com.javarush.alimova.configure.Creator;
 import com.javarush.alimova.configure.SessionCreator;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 
+import static org.hibernate.cfg.JdbcSettings.*;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ConfigurationIT {
@@ -13,18 +16,27 @@ public class ConfigurationIT {
     public static final String CHANGELOG_FILE = "db/changelog_test.xml";
     public static final String CHANGELOG_FILE_NAME = "CHANGELOG_FILE";
 
+    private static final JdbcDatabaseContainer<?> CONTAINER;
+
+    public static final String DOCKER_IMAGE_NAME = "postgres:16.3";
+
     protected final ConfigurationDB configurationDB;
-    protected final ApplicationProperties applicationProperties;
+    protected static final ApplicationProperties applicationProperties;
     protected final SessionCreator sessionCreator;
 
-//    static {
-//        ApplicationProperties properties = Creator.getComponent(ApplicationProperties.class);
-//        properties.setProperty(CHANGELOG_FILE_NAME, CHANGELOG_FILE);
-//    }
+    static {
+        CONTAINER = new PostgreSQLContainer<>(DOCKER_IMAGE_NAME);
+        CONTAINER.start();
 
-    public ConfigurationIT() {
         applicationProperties = Creator.getComponent(ApplicationProperties.class);
         applicationProperties.setProperty(CHANGELOG_FILE_NAME, CHANGELOG_FILE);
+        applicationProperties.setProperty(JAKARTA_JDBC_URL, CONTAINER.getJdbcUrl());
+        applicationProperties.setProperty(JAKARTA_JDBC_USER, CONTAINER.getUsername());
+        applicationProperties.setProperty(JAKARTA_JDBC_PASSWORD, CONTAINER.getPassword());
+    }
+
+    public ConfigurationIT() {
+
         configurationDB = Creator.getComponent(ConfigurationDB.class);
         try {
             configurationDB.startInitDB();
